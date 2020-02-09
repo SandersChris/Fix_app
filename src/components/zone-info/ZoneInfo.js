8fc7f4906fbdffb82061264e3c616a950ecfa97a
Chris Sanders <christopherdksanders@gmail.com>
2020-02-09 15:21:27 -0800

    added basic info block positioning and styling

--- src/components/zone-info/ZoneInfo.js
+++ src/components/zone-info/ZoneInfo.js
@@ -4,22 +4,36 @@ import './zone-info.styles.css'
 
 const ZoneInfo = () => {
     return (
-        <div className='zone-info-container'>
-            <div className='zone-header-container'>
-                <h4 className='zone-info-title'>ZONE PHILOSOPHY</h4>
-                <p className='zone-info-tag'>
-                    Calorie zones allow anyone to easily conceptualize
-                    weight loss.
-                </p>
-                <p className='zone-info-explainer'>
-                    Calorie Zones utilize the 1990 revised 
-                    Harris-Benedict equation for BMR to create an
-                    intuitive look into calorie intake and its 
-                    effect on weight. Believe it or not, this is 
-                    one of the only weight loss tools you'll ever
-                    need.
-                </p>
+        <div className='main-container'>
+            <div className='zone-info-container'>
+                <div className='zone-header-container'>
+                    <h4 className='zone-info-title'>ZONE PHILOSOPHY</h4>
+                    <p className='zone-info-tag'>
+                        Calorie zones allow anyone to easily conceptualize
+                        weight loss.
+                    </p>
+                    <p className='zone-info-explainer'>
+                        Calorie Zones utilize the 1990 revised 
+                        Harris-Benedict equation for BMR to create an
+                        intuitive look into calorie intake and its 
+                        effect on weight. Believe it or not, this is 
+                        one of the only weight loss tools you'll ever
+                        need.
+                    </p>
+                </div>
             </div>
+                <div className='red-zone-container'>
+                    <div></div>
+                </div>
+                <div className='yellow-zone-container'>
+                    <div></div>
+                </div>
+                <div className='green-zone-container'>
+                    <div></div>
+                </div>
+                <div className='double-red-zone-container'>
+                    <div></div>
+                </div>
         </div>
     )
 }
